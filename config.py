fffc47967ee0b8ef7d322280c7d5e350a62c73e0
BlindX08 <hayli@hostelness.com>
2024-09-02 02:23:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7532541508:AAGWjho4rBTjXdPXmMd14aQ29BZXivcxM94")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("unknown_2863")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5139930247")
 
 
 
